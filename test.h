bf99673b935aed566b3ab2bbf518ecdcea3b8273
ducng <ducngyn2211@icloud.com>
2024-10-07 15:13:38 +0200

    Merge remote-tracking branch 'origin/main'

---
+++ test.h
@@ -0,0 +1,14 @@
+//
+// Created by ducng on 07.10.2024.
+//
+
+#ifndef TEST3_TEST_H
+#define TEST3_TEST_H
+
+
+class test {
+
+};
+
+
+#endif //TEST3_TEST_H
---
+++ tests.c
@@ -0,0 +1,10 @@
+//
+// Created by ducng on 07.10.2024.
+//
+#include <stdio.h>
+
+int main (){
+
+    printf("hello world");
+
+}
